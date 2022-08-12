b4dd4c1b4c8c52b817056804ee64e351861ae3ad
danychaddad <dany.chaddad@gmail.com>
2022-08-12 14:43:37 +0300

    Added UI to pedestal prefab

--- Assets/Scripts/Pedestal.cs
+++ Assets/Scripts/Pedestal.cs
@@ -10,11 +10,16 @@ public class Pedestal : MonoBehaviour
     private Animator objectAnimator;
     [Range(0, 5), SerializeField]
     private float animationSpeed = 1.0f;
-
+    private Canvas canvas;
+    private Camera mainCamera;
 
     private void Start()
     {
+        mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        canvas = GetComponentInChildren<Canvas>();
+        canvas.worldCamera = mainCamera;
         displayedObject = Instantiate(objectPrefab, objectPlaceholder.transform.position, objectPrefab.transform.rotation);
+
         objectAnimator = displayedObject.GetComponent<Animator>();
         objectAnimator.speed = animationSpeed;
     }
@@ -26,5 +31,9 @@ public class Pedestal : MonoBehaviour
     private void Update()
     {
         UpdateSpeed();
+    }
+    public void SetAnimationSpeed(float value)
+    {
+        animationSpeed = value;
     }
 }
